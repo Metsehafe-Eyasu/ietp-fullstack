d58efd5e01f3a884b2535db0ae88d9a863b48cfb
Metsehafe Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2024-01-15 09:37:02 +0300

    log: check production error

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,13 +3,19 @@ import { initAdmin } from "@/lib/db/firebaseAdmin";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: any) {
+  console.log('start post process')
   const admin = await initAdmin();
+  console.log('initalized admin')
   const db = admin.database();
+  console.log('admin database connected')
   
   const ref = db.ref("steps");
+  console.log('referenced steps')
   const body = await req.json();
+  console.log('request body', body)
   body.timestamp = Date.now();
 
   await ref.push(body);
+  console.log('pushed to database')
   return NextResponse.json(body);
 }
